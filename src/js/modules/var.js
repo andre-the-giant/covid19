d596bcfa0d734489a0ac8eb16a5258ccc7d922ef
Collin <acollin@TOR-051450.pmcorp.postmedia.com>
2021-03-15 10:40:18 -0400

    Fix bad variable

--- src/js/modules/var.js
+++ src/js/modules/var.js
@@ -1,7 +1,7 @@
 const appVars = {
   API_URL: 'https://api.covid19api.com',
   ENDPOINT_LIST: '/summary',
-  ENDPOINT_COUNTRY: '/total/country/',
+  ENDPOINT_COUNTRY: '/total/country',
   LOCALE_STRING: 'us-US',
   STORAGE: 'LOCAL_STORAGE',
   mainContainer: document.querySelector('main')
